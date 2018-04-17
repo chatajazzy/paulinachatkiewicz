58cb6bc893c749cd0e2e3611316545e4461896bd
Unknown <chatajazzy@interia.pl>
2018-04-17 16:34:00 +0200

    Add socials component to footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Header from './components/Header';
+import Socials from './components/Socials';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
+        <Socials />
       </div>
     );
   }
