dc20c79dce25708f195ba149b574cd9f780c9dc1
Unknown <chatajazzy@interia.pl>
2018-04-17 18:56:58 +0200

    Add starter website pages components

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const About = props => {
+  return (
+    <div>
+      <section className="about__info">
+        <header>O mnie</header>
+        <p>blablabla</p>
+      </section>
+      <section className="about__skills">
+        <header>Umiejętności</header>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default About;
