c498e1095edbe7f940e16666c370ed511caa6f75
Unknown <chatajazzy@interia.pl>
2018-04-18 19:11:21 +0200

    Add styles to homepave

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Home = props => {
-  return (
-    <main>
-      <h2>Home</h2>
-    </main>
-  );
-};
-
+class Home extends React.Component {
+  componentDidMount() {
+    console.log('dsd', document.querySelector('.nav'));
+    document.querySelector('.nav').classList.add('nav--home');
+  }
+  componentWillUnmount() {
+    document.querySelector('.nav').classList.remove('nav--home');
+  }
+  render() {
+    return (
+      <div className="main__container main__container--full-page">
+        <section className="main__section main__section--full-page">
+          <div className="home__hero-img" />
+        </section>
+        <section className="main__section main__section--hero">
+          <div className="home__hero">
+            <p className="home__hero-title">Paulina Chatkiewicz</p>
+            <p className="home__hero-subtitle">Frontend developer</p>
+            <NavLink
+              exact
+              to="/works"
+              className="home__hero-cta"
+              activeClassName="active"
+            >
+              Sprawdź
+            </NavLink>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 export default Home;
