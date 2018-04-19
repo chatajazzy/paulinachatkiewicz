3640c457299081083b1c352b6cf7b0834ea9db75
Unknown <chatajazzy@interia.pl>
2018-04-19 17:26:55 +0200

    Add basic components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Socials from './components/Socials';
+import NotFound from './components/NotFound';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Works from './components/Works';
+
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Router>
+          <div className="container">
+            <Header />
+            <main className="main">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/works" component={Works} />
+                <Route path="/contact" component={Contact} />
+                <Route component={NotFound} />
+              </Switch>
+            </main>
+          </div>
+        </Router>
+        <Socials />
       </div>
     );
   }
