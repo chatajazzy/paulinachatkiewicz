0d6a8e1a553f44d430d6aa2ce973a5601c46fa66
Unknown <chatajazzy@interia.pl>
2018-04-23 17:07:09 +0200

    Add basic structure to details component

--- src/components/WorksItemDetails.js
+++ src/components/WorksItemDetails.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class WorksItemDetails extends React.Component {
   render() {
+    const currentWorksId = this.props.match.params.worksId;
+    const currentItemInfo = this.props.itemInfo.find(function(obj) {
+      return obj.id === currentWorksId;
+    });
     return (
-      <div>
-        <h3>WorksItemDetails</h3>
+      <div className="works-item-details">
+        <Link to="/works" className="works-item-details__link">
+          <div className="works-item-details__close">
+            <span className="works-item-details__close-bar" />
+            <span className="works-item-details__close-bar" />
+          </div>
+        </Link>
+        <div className="works-item-details__content">
+          <h3 className="works-item-details__title">{currentItemInfo.title}</h3>
+          <p className="works-item-details__subtitle">
+            {currentItemInfo.subtitle}
+          </p>
+          <img src={currentItemInfo.src} alt={currentItemInfo.title} />
+        </div>
       </div>
     );
   }
