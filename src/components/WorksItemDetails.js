fd680b8cdced2a04bd38cc5a12ec1a3dc57b09c0
Unknown <chatajazzy@interia.pl>
2018-04-24 12:44:49 +0200

    Add gsap animation to works item details component elements

--- src/components/WorksItemDetails.js
+++ src/components/WorksItemDetails.js
@@ -1,7 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TweenMax, TimelineMax, CustomEase } from 'gsap';
 
 class WorksItemDetails extends React.Component {
+  componentDidMount() {
+    //TODO: refactor needed
+    const targetObject = document.querySelector('.works-item-details__title');
+    const targetObject2 = document.querySelector(
+      '.works-item-details__subtitle'
+    );
+    const targetObject3 = document.querySelector('.works-item-details__img');
+    const targetObject4 = document.querySelector(
+      '.works-item-details__paragraph-list'
+    );
+    const targetObject5 = document.querySelector('.works-item-details__btns');
+
+    const stagingTimeline = new TimelineMax();
+
+    stagingTimeline
+      .from(targetObject, 1, { y: 30, opacity: 0 })
+      .from(targetObject2, 1, { y: 60, opacity: 0 })
+      .from(targetObject3, 1, { y: -30, opacity: 0 })
+      .from(targetObject4, 1, { y: 60, opacity: 0 })
+      .from(targetObject5, 1, { opacity: 0 })
+
+      .add('end', 2)
+
+      .to(targetObject, 3, { y: 0, opacity: 1 }, 'end')
+      .to(targetObject2, 3, { y: 0, opacity: 1 }, 'end')
+      .to(targetObject3, 3, { y: 0, opacity: 1 }, 'end')
+      .to(targetObject3, 3, { y: 0, opacity: 1 }, 'end')
+      .to(targetObject5, 3, { opacity: 1 }, 'end');
+
+    stagingTimeline.play();
+  }
+
   render() {
     const currentWorksId = this.props.match.params.worksId;
     const currentItemInfo = this.props.itemInfo.find(function(obj) {
