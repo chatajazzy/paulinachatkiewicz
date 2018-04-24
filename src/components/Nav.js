3e859eacfe38b0152090d85b10fdd76998e5d181
Unknown <chatajazzy@interia.pl>
2018-04-24 13:23:53 +0200

    Add close on escape to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import CloseOnEscape from 'react-close-on-escape';
+
 import { navLinks } from '../data/nav-links';
 
 class Nav extends React.Component {
@@ -10,23 +12,25 @@ class Nav extends React.Component {
   render() {
     return (
       <nav className={this.props.mobileMenuVisible ? 'nav nav--open' : 'nav'}>
-        <ul className="nav__list" onClick={this.props.handleMenu}>
-          {this.navLinks.map(function(navLink, index) {
-            return (
-              <li className="nav__list-item" key={`navLink${index}`}>
-                <NavLink
-                  exact
-                  to={navLink.link}
-                  className="nav__list-link"
-                  activeClassName="active"
-                >
-                  <span className="nav__item-name">{navLink.name}</span>
-                  <span className="nav__item-label">{navLink.title}</span>
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
+        <CloseOnEscape onEscape={this.props.handleMenu}>
+          <ul className="nav__list" onClick={this.props.handleMenu}>
+            {this.navLinks.map(function(navLink, index) {
+              return (
+                <li className="nav__list-item" key={`navLink${index}`}>
+                  <NavLink
+                    exact
+                    to={navLink.link}
+                    className="nav__list-link"
+                    activeClassName="active"
+                  >
+                    <span className="nav__item-name">{navLink.name}</span>
+                    <span className="nav__item-label">{navLink.title}</span>
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </CloseOnEscape>
       </nav>
     );
   }
