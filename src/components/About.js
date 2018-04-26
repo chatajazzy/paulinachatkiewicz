834d2a9ebab7a82c1f199885fa8e2b436501d74d
Unknown <chatajazzy@interia.pl>
2018-04-26 22:08:55 +0200

    Refactor about.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,13 @@ import { TimelineMax } from 'gsap';
 import Grid from './Grid';
 import Paulina from '../img/paulina.jpg';
 
+import { aboutParagraphs } from '../data/about-paragraphs';
+
 class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.aboutParagraphs = aboutParagraphs;
+  }
   componentDidMount() {
     //TODO: refactor needed
     const targetObject1 = document.querySelector('.main__content-title');
@@ -16,13 +22,7 @@ class About extends React.Component {
     stagingTimeline
       .from(targetObject1, 1, { y: 30, opacity: 0 })
       .from(targetObject2, 1, { y: 60, opacity: 0 })
-      .from(targetObject3, 1, { y: -30, opacity: 0 })
-
-      .add('end', 2)
-
-      .to(targetObject1, 3, { y: 0, opacity: 1 }, 'end')
-      .to(targetObject2, 3, { y: 0, opacity: 1 }, 'end')
-      .to(targetObject3, 3, { y: 0, opacity: 1 }, 'end');
+      .from(targetObject3, 1, { y: -30, opacity: 0 }, '-=0.5');
 
     stagingTimeline.play();
   }
@@ -36,52 +36,32 @@ class About extends React.Component {
           </header>
           <div className="main__inner-wrapper main__inner-wrapper--about">
             <ul className="main__paragraph-list">
-              <li>
-                <p className="main__content-paragraph">
-                  Obecnie zajmuję się tworzeniem stron internetowych, a
-                  właściwie uczę się robić je jak najlepiej się da. Choć
-                  front-endem interesuję się już od jakiegoś czasu to realizuję
-                  się w nim od około 6 miesięcy.
-                </p>
-                <p className="main__content-paragraph">
-                  Przez ten czas poznałam wiele zagadnień związanych z budową
-                  układu strony oraz jego dostosowywaniem do urządzeń mobilnych.
-                  Wiem co to RWD i jak ważne jest dzisiaj jego wdrożenie.
-                </p>
-              </li>
-              <li>
-                <p className="main__content-paragraph">
-                  Nie posiadam na dany moment komercyjnego doświadczenia jako
-                  front end developer, ale chcę to w najbliższym czasie zmienić.
-                </p>
-                <p className="main__content-paragraph">
-                  Jako osoba początkująca mogę zaoferować pełne zaangażowanie w
-                  zlecone zadania, szybkie przyswajanie nowej wiedzy oraz świeże
-                  spojrzenie na zagadnienia.
-                </p>
-              </li>
-              <li>
-                <p className="main__content-paragraph">
-                  Realizowane projekty zawsze staram się wdrażać biorąc pod
-                  uwagę wszelkie aspekty związane z ich poprawnością, wiernym
-                  oddaniem wyglądu oraz optymalizacją pod kątem szybkości
-                  działania.
-                </p>
-              </li>
-              <li>
-                <p className="main__content-paragraph">
-                  Jako osoba początkująca mogę zaoferować pełne zaangażowanie w
-                  zlecone zadania, szybkie przyswajanie nowej wiedzy oraz świeże
-                  spojrzenie na zagadnienia.
-                </p>
-                <figure className="about-photo">
-                  <img
-                    src={Paulina}
-                    alt="Paulina Chatkiewicz"
-                    className="about-photo__img"
-                  />
-                </figure>
-              </li>
+              {this.aboutParagraphs.map((item, index) => {
+                if (index !== this.aboutParagraphs.length - 1) {
+                  return (
+                    <li key={`aboutParagraphs${index}`}>
+                      <p className="main__content-paragraph">
+                        {item.paragraph}
+                      </p>
+                    </li>
+                  );
+                } else {
+                  return (
+                    <li key={`aboutParagraphs${index}`}>
+                      <p className="main__content-paragraph">
+                        {item.paragraph}
+                      </p>
+                      <figure className="about-photo">
+                        <img
+                          src={Paulina}
+                          alt="Paulina Chatkiewicz"
+                          className="about-photo__img"
+                        />
+                      </figure>
+                    </li>
+                  );
+                }
+              })}
             </ul>
           </div>
         </section>
---
+++ src/data/about-paragraphs.js
@@ -0,0 +1,27 @@
+export const aboutParagraphs = [
+  {
+    paragraph: `Obecnie zajmuję się tworzeniem stron internetowych, a
+    właściwie uczę się robić je jak najlepiej się da. Choć
+    front-endem interesuję się już od jakiegoś czasu to realizuję
+    się w nim od około 6 miesięcy. Przez ten czas poznałam wiele zagadnień związanych z budową
+    układu strony oraz jego dostosowywaniem do urządzeń mobilnych.
+    Wiem co to RWD i jak ważne jest dzisiaj jego wdrożenie.`
+  },
+  {
+    paragraph: `Nie posiadam na dany moment komercyjnego doświadczenia jako
+    front end developer, ale chcę to w najbliższym czasie zmienić.Jako osoba początkująca mogę zaoferować pełne zaangażowanie w
+    zlecone zadania, szybkie przyswajanie nowej wiedzy oraz świeże
+    spojrzenie na zagadnienia.`
+  },
+  {
+    paragraph: `Realizowane projekty zawsze staram się wdrażać biorąc pod
+    uwagę wszelkie aspekty związane z ich poprawnością, wiernym
+    oddaniem wyglądu oraz optymalizacją pod kątem szybkości
+    działania.`
+  },
+  {
+    paragraph: `Jako osoba początkująca mogę zaoferować pełne zaangażowanie w
+    zlecone zadania, szybkie przyswajanie nowej wiedzy oraz świeże
+    spojrzenie na zagadnienia.`
+  }
+];
