13eadcbf283686bdca769cbf2d99103b9d92bd4b
Unknown <chatajazzy@interia.pl>
2018-05-08 22:49:36 +0200

    Change animation to css instead of react transitions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
-import { TweenLite } from 'gsap';
-import { Transition } from 'react-transition-group';
-import TransitionReplace from 'react-transition-replace';
 import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -15,24 +12,6 @@ import ProgressBar from './components/ProgressBar';
 import './App.scss';
 import BlogPostPage from './pages/BlogPostPage';
 
-const animIn = node => {
-  TweenLite.set(node, {
-    opacity: 0
-  });
-  TweenLite.to(node, 0.5, {
-    opacity: 1
-  });
-};
-
-const animOut = node => {
-  TweenLite.set(node, {
-    opacity: 1
-  });
-  TweenLite.to(node, 0.5, {
-    opacity: 0
-  });
-};
-
 class App extends Component {
   state = {
     mobileMenuVisible: false,
@@ -42,7 +21,6 @@ class App extends Component {
     if (!this.state.wasPreloaderShowed) {
       setTimeout(() => {
         document.body.classList.add('page-loaded');
-
         this.handlePreloader();
       }, 2000);
     }
@@ -56,47 +34,31 @@ class App extends Component {
   };
   handleMenu = () => {
     if (this.state.mobileMenuVisible === false) {
-      this.setState({
-        mobileMenuVisible: true
-      });
+      this.setState({ mobileMenuVisible: true });
     } else {
-      this.setState({
-        mobileMenuVisible: false
-      });
+      this.setState({ mobileMenuVisible: false });
     }
   };
   render() {
     return (
-      <TransitionReplace>
-        <Transition
-          key={this.props.location.pathname}
-          timeout={500}
-          mountOnEnter={true}
-          onEnter={animIn}
-          onExit={animOut}
-        >
-          <div className="page-container">
-            <Header
-              mobileMenuVisible={this.state.mobileMenuVisible}
-              handleMenu={this.handleMenu}
-              wasPreloaderShowed={this.state.wasPreloaderShowed}
-            />
-            {/*<Preloader />*/}
-            <main className="main">
-              <Switch location={this.props.location}>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route path="/works" component={Works} />
-                <Route exact path="/blog" component={Blog} />
-                <Route path="/blog/:id" component={BlogPostPage} />
-
-                <Route exact path="/contact" component={Contact} />
-                <Route component={NotFound} />
-              </Switch>
-            </main>
-          </div>
-        </Transition>
-      </TransitionReplace>
+      <div className="page-container">
+        <Header
+          mobileMenuVisible={this.state.mobileMenuVisible}
+          handleMenu={this.handleMenu}
+          wasPreloaderShowed={this.state.wasPreloaderShowed}
+        />
+        <main className="main">
+          <Switch location={this.props.location}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route path="/works" component={Works} />
+            <Route exact path="/blog" component={Blog} />
+            <Route path="/blog/:id" component={BlogPostPage} />
+            <Route exact path="/contact" component={Contact} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+      </div>
     );
   }
 }
