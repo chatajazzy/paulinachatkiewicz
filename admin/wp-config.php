5f5bfe9c66727f7cc63c5c867ca8a0f15afdffa4
Unknown <chatajazzy@interia.pl>
2018-05-08 22:26:09 +0200

    Change admin dir name

--- admin/wp-config.php
+++ admin/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'admin.paulinachatkiewicz.test');
+define('DB_NAME', '1117301_GzM');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '1117301_GzM');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'II7z0oPbpO6gYn');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'userdb1');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
