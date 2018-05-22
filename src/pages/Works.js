4d3749a3695362cd04dd5e219f20c324df2ad2e6
Unknown <chatajazzy@interia.pl>
2018-05-22 14:22:36 +0200

    Add mockup images to works page

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -1,54 +1,43 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import {
+  Route
+} from 'react-router-dom';
 
 import WorksSlider from '../components/WorksSlider';
 
-// import agencysite from '../img/paulina.jpg';
-// import graxposite from '../img/graxposite.jpg';
-// import linearsite from '../img/linearsite.jpg';
+import expensify from '../img/expensify-mockup.jpg';
+import fluxStore from '../img/fluxstore-mockup.jpg';
 
 import WorksItemDetails from './WorksItemDetails';
+
 class Works extends React.Component {
   placeholderImg = 'http://placehold.it/800x800';
-  items = [
-    {
-      id: 'agencysite',
-      title: 'agencysite',
-      subtitle: 'Statyczna strona www',
+  items = [{
+      id: 'expensify',
+      title: 'expensify',
+      subtitle: 'Aplikacja do zarządzania wydatkami',
       content: 'blablablabla',
-      src: this.placeholderImg,
-      linkCode: 'https://github.com/chatajazzy/graxpo',
-      linkLive: 'https://chatajazzy.github.io/graxpo'
+      src: expensify,
+      linkCode: 'https://github.com/chatajazzy/expensify',
+      linkLive: 'https://chatajazzy-expensify.herokuapp.com/'
     },
     {
-      id: 'graxposite',
-      title: 'graxposite',
-      subtitle: 'Statyczna strona www',
+      id: 'fluxstore',
+      title: 'fluxstore',
+      subtitle: 'Prosty sklep internetowy',
       content: 'blablablabla',
-      src: this.placeholderImg,
-      linkCode: 'https://github.com/chatajazzy/graxpo',
-      linkLive: 'https://chatajazzy.github.io/graxpo'
-    },
-    {
-      id: 'linearsite',
-      title: 'linearsite',
-      subtitle: 'Statyczna strona www',
-      content: 'blablablabla',
-      src: this.placeholderImg,
-      linkCode: 'https://github.com/chatajazzy/graxpo',
-      linkLive: 'https://chatajazzy.github.io/graxpo'
+      src: fluxStore,
+      linkCode: 'https://chatajazzy.github.io/flux-demo',
+      linkLive: 'https://github.com/chatajazzy/flux-demo'
     }
   ];
   render() {
-    return (
+    return ( 
       <div className="main__wrapper">
-        <WorksSlider items={this.items} />
-        <Route
-          path="/works/:worksId"
-          render={props => (
-            <WorksItemDetails itemInfo={this.items} {...props} />
-          )}
-        />
+        <WorksSlider items={this.items} /> 
+        <Route path = "/works/:worksId" render={props => ( 
+          <WorksItemDetails itemInfo={this.items} { ...props} />)}
+        /> 
       </div>
     );
   }
